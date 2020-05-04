f0ae72a7b01b92971af65eeb33e28c4705b63443
Paulo Victor <paulovictor237@gmail.com>
2020-05-04 08:49:06 -0300

    Atualização do instalador MoveIt-Full
    atualização do instalador do Moveit-Ros-Full

--- instalador_moveit_completo.sh
+++ instalador_moveit_completo.sh
@@ -2,6 +2,9 @@
 
 echo "Aguarde a instalação"
 
+cat bashpv.sh >> ~/.bashrc
+source ~/.bashrc
+
 #Setup your sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Set up your keys
@@ -11,7 +14,7 @@ sudo apt update
 sudo apt install ros-melodic-desktop-full
 apt search ros-melodic
 #Environment setup
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+# echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 #Dependencies for building packages
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
@@ -37,9 +40,11 @@ wstool update -t .
 cd ~/ws_moveit/src
 rosdep install -y --from-paths . --ignore-src --rosdistro melodic
 cd ~/ws_moveit
+source ~/.bashrc
 catkin config --extend /opt/ros/melodic --cmake-args -DCMAKE_BUILD_TYPE=Release
 catkin build
-source ~/ws_moveit/devel/setup.bash
-echo 'source ~/ws_moveit/devel/setup.bash' >> ~/.bashrc
+# source ~/ws_moveit/devel/setup.bash
+# echo 'source ~/ws_moveit/devel/setup.bash' >> ~/.bashrc
+source ~/.bashrc
 
 echo "Instalação completa"
